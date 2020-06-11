asdddd1111/hospital

Добавьте файлы проекта.

NewAmsterdamHospital/Properties/AssemblyInfo.cs
@@ -1,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется следующим образом
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных с этой сборкой.
+[assembly: AssemblyTitle("NewAmsterdamHospital")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("NewAmsterdamHospital")]
+[assembly: AssemblyCopyright("© Microsoft, 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False в параметре ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM.  Если требуется обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение true для требуемого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для ID библиотеки типов typelib, если этот проект видим для COM
+[assembly: Guid("1b3b0909-61f0-4e1b-bd08-c87a7fe1f361")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      основной номер версии;
+//      Дополнительный номер версии
+//      номер сборки;
+//      редакция.
+//
+// Можно задать все значения или принять номер редакции и номер сборки по умолчанию,
+// используя "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

NewAmsterdamHospital/Service/ScheduleForDoctor.cs
@@ -1,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NewAmsterdamHospital.Models;
+
+namespace NewAmsterdamHospital.Service
+{
+    public static class ScheduleForDoctor
+    {
+        public static void Add( int indexForDoctorSpecialtie, DateTime date)
+        {
+            UserContext db = new UserContext();
+            if (db.Receptions.Count() == 0)
+            {
+                int timeOfAppointment = 30;
+                int dayOfWork = 1;
+                int daysInWeek = 7;
+                var twoWeeks = date.AddDays(daysInWeek);
+                int sunday = 0;
+                int saturday = 6;
+                int idDateReception = 1;
+                while (twoWeeks >=date)
+                {
+                    var beginTime = db.DataForContacts.FirstOrDefault(x => x.Id == 1).StartWorkTime;
+                    var endTime = beginTime.AddMinutes(timeOfAppointment);
+                    if ((int)date.DayOfWeek != sunday & (int)date.DayOfWeek != saturday)
+                    {
+                        while (endTime <= db.DataForContacts.FirstOrDefault(x => x.Id == 1).EndWorkTime)
+                        {
+                            db.Receptions.Add(new Reception { TimeName = $"{beginTime.ToString("HH:mm")}-{endTime.ToString("HH:mm")}", DateReceptionId = idDateReception, IsUse = false });
+                            beginTime = beginTime.AddMinutes(timeOfAppointment);
+                            endTime = endTime.AddMinutes(timeOfAppointment);
+                        }
+                        db.DateReceptions.Add(new DateReception { Id = idDateReception, DateName = date.ToString("dd MMMM"), DoctorSpecialtyId = indexForDoctorSpecialtie, Date = date });
+                    }
+                    date = date.AddDays(dayOfWork);
+                    idDateReception++;
+                }
+                db.SaveChanges();
+            }
+            else
+            {
+                int timeOfAppointment = 30;
+                int dayOfWork = 1;
+                int daysInWeek = 7;
+                var twoWeeks = date.AddDays(daysInWeek);
+                int sunday = 0;
+                int saturday = 6;
+                int idDateReception = db.DateReceptions.Max(p => p.Id) + 1;
+                while (twoWeeks >= date)
+                {
+                    var beginTime = db.DataForContacts.FirstOrDefault(x => x.Id == 1).StartWorkTime;
+                    var endTime = beginTime.AddMinutes(timeOfAppointment);
+                    if ((int)date.DayOfWeek != sunday & (int)date.DayOfWeek != saturday)
+                    {
+                        while (endTime <= db.DataForContacts.FirstOrDefault(x => x.Id == 1).EndWorkTime)
+                        {
+                            db.Receptions.Add(new Reception { TimeName = $"{beginTime.ToString("HH:mm")}-{endTime.ToString("HH:mm")}", DateReceptionId = idDateReception, IsUse = false });
+                            beginTime = beginTime.AddMinutes(timeOfAppointment);
+                            endTime = endTime.AddMinutes(timeOfAppointment);
+                        }
+                        db.DateReceptions.Add(new DateReception { Id = idDateReception, DateName = date.ToString("dd MMMM"), DoctorSpecialtyId = indexForDoctorSpecialtie, Date = date });
+                    }
+                    date = date.AddDays(dayOfWork);
+                    idDateReception++;
+                }
+                db.SaveChanges();
+            }
+            
+        }
+    }
+}

asdddd1111/example

NewAmsterdamHospital/Properties/AssemblyInfo.cs
@@ -1,0 +1,38 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется следующим образом
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных с этой сборкой.
+[assembly: AssemblyTitle("NewAmsterdamHospital")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("NewAmsterdamHospital")]
+[assembly: AssemblyCopyright("© Microsoft, 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False в параметре ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM.  Если требуется обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение true для требуемого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для ID библиотеки типов typelib, если этот проект видим для COM
+[assembly: Guid("1b3b0909-61f0-4e1b-bd08-c87a7fe1f361")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      основной номер версии;
+//      Дополнительный номер версии
+//      номер сборки;
+//      редакция.
+//
+// Можно задать все значения или принять номер редакции и номер сборки по умолчанию,
+// используя "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("")]
+

NewAmsterdamHospital/Service/ScheduleForDoctor.cs
@@ -1,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NewAmsterdamHospital.Models;
+
+namespace NewAmsterdamHospital.Service
+{
+    public static class ScheduleForDoctor
+    {
+        public static void Add( int indexForDoctorSpecialtie, DateTime date)
+        {
+            UserContext db = new UserContext();
+            if (db.Receptions.Count() == 0)
+            {
+                int timeOfAppointment = 30;
+                int dayOfWork = 1;
+                int daysInWeek = 7;
+                var week = date.AddDays(daysInWeek);
+                int sunday = 0;
+                int saturday = 6;
+                int idDateReception = 1;
+                while (week >= date)
+                {
+                    var beginTime = db.DataForContacts.FirstOrDefault(x => x.Id == 1).StartWorkTime;
+                    var endTime = beginTime.AddMinutes(timeOfAppointment);
+                    if ((int)date.DayOfWeek != sunday & (int)date.DayOfWeek != saturday)
+                    {
+                        while (endTime <= db.DataForContacts.FirstOrDefault(x => x.Id == 1).EndWorkTime)
+                        {
+                            db.Receptions.Add(new Reception { TimeName = $"{beginTime.ToString("HH:mm")}-{endTime.ToString("HH:mm")}", DateReceptionId = idDateReception, IsUse = false });
+                            beginTime = beginTime.AddMinutes(timeOfAppointment);
+                            endTime = endTime.AddMinutes(timeOfAppointment);
+                        }
+                        db.DateReceptions.Add(new DateReception { Id = idDateReception, DateName = date.ToString("dd MMMM"), DoctorSpecialtyId = indexForDoctorSpecialtie, Date = date });
+                    }
+                    date = date.AddDays(dayOfWork);
+                    idDateReception++;
+                }
+                db.SaveChanges();
+            }
+            else
+            {
+                int timeOfAppointment = 30;
+                int dayOfWork = 1;
+                int daysInWeek = 7;
+                var week = date.AddDays(daysInWeek);
+                int sunday = 0;
+                int saturday = 6;
+                int idDateReception = db.DateReceptions.Max(p => p.Id) + 1;
+                while (week >= date)
+                {
+                    var beginTime = db.DataForContacts.FirstOrDefault(x => x.Id == 1).StartWorkTime;
+                    var endTime = beginTime.AddMinutes(timeOfAppointment);
+                    if ((int)date.DayOfWeek != sunday & (int)date.DayOfWeek != saturday)
+                    {
+                        while (endTime <= db.DataForContacts.FirstOrDefault(x => x.Id == 1).EndWorkTime)
+                        {
+                            db.Receptions.Add(new Reception { TimeName = $"{beginTime.ToString("HH:mm")}-{endTime.ToString("HH:mm")}", DateReceptionId = idDateReception, IsUse = false });
+                            beginTime = beginTime.AddMinutes(timeOfAppointment);
+                            endTime = endTime.AddMinutes(timeOfAppointment);
+                        }
+                        db.DateReceptions.Add(new DateReception { Id = idDateReception, DateName = date.ToString("dd MMMM"), DoctorSpecialtyId = indexForDoctorSpecialtie, Date = date });
+                    }
+                    date = date.AddDays(dayOfWork);
+                    idDateReception++;
+                }
+                db.SaveChanges();
+            }
+            
+        }
+    }
+}